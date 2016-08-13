e651f5c0d666186b670ee8f03452b1d181074176
Louiefigz <figueroa.aaron@gmail.com>
2016-08-13 10:43:23 -0400

    adding app.js file. Adding recipe controller stubbed information

---
+++ app/assets/javascripts/app/recipes/recipes.controller.js
@@ -0,0 +1,12 @@
+function RecipesController(){
+var vm = this;
+  vm.recipes =[
+    {
+      name: pad thai
+    }
+  ]
+}
+
+angular
+  .module('app')
+  .controller('RecipesController', RecipesController);
