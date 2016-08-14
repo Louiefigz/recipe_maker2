0a75fac818700f26acef3abeb1cb591b0437502f
Louiefigz <figueroa.aaron@gmail.com>
2016-08-14 11:55:38 -0400

    New ingredient for recipes form sending back correct data to controller

--- app/assets/javascripts/app/recipes/recipes.controller.js
+++ app/assets/javascripts/app/recipes/recipes.controller.js
@@ -2,6 +2,7 @@ function RecipesController(){
 var vm = this;
   vm.recipes =[
     {
+      id: 1,
       name: 'pad thai',
       ingredients: [
       {
@@ -19,25 +20,32 @@ var vm = this;
       ]
     },
     {
+      id:2,
       name: 'lemon chicken'
     },
     {
+      id:3,
       name: 'meatloaf'
     },
     {
+      id:4,
       name: 'Penne al a vodka'
     }
 
   ]
 
-var id = window.location.pathname.split('/')[window.location.pathname.split('/').length - 1]
+var id = window.location.pathname.split('/')[window.location.pathname.split('/').length - 1];
   this.recipeShowPage = function(){
     vm.recipe = vm.recipes[id-1];
   }
 
   this.newIngredient = function(data){
     debugger;
-
+//Why is is that I can not access vm.recipes in the console?
+  }
+  this.recipeShow = function(data){
+  debugger;
+    vm.recipe = this.recipes[data-1];
   }
 };
 
