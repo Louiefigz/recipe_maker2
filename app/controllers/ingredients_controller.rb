b640326ff3e0bba668f48c9bc7de9cb9bc7a4041
Louiefigz <figueroa.aaron@gmail.com>
2016-08-14 17:35:37 -0400

    creating route to ingredients index page

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,9 +1,14 @@
 class IngredientsController < ApplicationController
   def index
     ingredients = Ingredient.all
-    render json: ingredients
+
+    respond_to do |format|
+      format.html
+      format.json { render json: ingredients}
+    end
   end
 
   def show
+    ingredient = Ingredient.find(params[:id])
   end
 end
