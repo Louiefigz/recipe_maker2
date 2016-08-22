0fd89291a05cdcd3b75b8c454ebdd4e9619be634
Louiefigz <figueroa.aaron@gmail.com>
2016-08-21 21:21:14 -0400

    ingredient factory is able to handle dynamic routes

--- app/assets/javascripts/app/ingredients/ingredient.factory.js
+++ app/assets/javascripts/app/ingredients/ingredient.factory.js
@@ -5,7 +5,7 @@ angular
   function Ingredient($resource){
 
     var Ingredient =[];
-    Ingredient = $resource('/ingredients', {},{
+    Ingredient = $resource('/ingredients/:id', {id:'@id'},{
       query: {method: 'GET', isArray:true},
        update: { method: 'PUT'},
        delete: { method: 'DELETE'}
