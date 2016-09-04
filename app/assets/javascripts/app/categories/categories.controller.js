9552ccd27ee63beb9a8ddd0cae6a1ef47b015a26
Louiefigz <figueroa.aaron@gmail.com>
2016-09-04 16:13:30 -0400

    Ability to now create a category in the Category index page

--- app/assets/javascripts/app/categories/categories.controller.js
+++ app/assets/javascripts/app/categories/categories.controller.js
@@ -6,8 +6,16 @@ function CategoriesController($scope, $http, $state, $stateParams, CategoryFacto
   CategoryFactory.query().$promise.then(function(data){
     vm.allCategories = data;
   });
+  vm.category = CategoryFactory.get({ id: $stateParams.id });
+  vm.newCategory = new CategoryFactory();
+  vm.createCategory = createCategory;
 
-  vm.category = CategoryFactory.get({ id: $stateParams.id })
+
+  function createCategory(){
+    vm.newCategory.$save(function (){
+      vm.allCategories= CategoryFactory.query();
+    });
+  }
 
 };
 
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,11 +5,21 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    category = Category.find_or_create_by(category_params)
   end
 
   def show
     category = Category.find(params[:id])
     render json: category
+  end
+
+  def update
+    # cat.recipe_categories.create(recipe_id: recipe.id)
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 
