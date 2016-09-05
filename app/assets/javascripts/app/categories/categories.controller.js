eedb5513b275efbd17e5379dd98b4199c83b8019
Louiefigz <figueroa.aaron@gmail.com>
2016-09-04 22:03:35 -0400

    Showing correct create and search text boxes in Categories index

--- app/assets/javascripts/app/categories/categories.controller.js
+++ app/assets/javascripts/app/categories/categories.controller.js
@@ -9,9 +9,28 @@ function CategoriesController($scope, $http, $state, $stateParams, CategoryFacto
   vm.category = CategoryFactory.get({ id: $stateParams.id });
   vm.newCategory = new CategoryFactory();
   vm.createCategory = createCategory;
+  vm.startAddCategory = startAddCategory;
+  vm.startDeleteButton = startDeleteButton;
+  vm.filterCategories = filterCategories;
   vm.showCreateCategory = false;
   vm.showSearch = true;
+  vm.showDeleteButton = false;
 
+  function filterCategories(){
+    vm.showCreateCategory = false;
+    vm.showSearch = true;
+  }
+
+
+  function startAddCategory(){
+    vm.showSearch = false;
+    vm.showCreateCategory = true;
+  }
+
+
+  function startDeleteButton(){
+    vm.showDeleteButton = !vm.showDeleteButton;
+  }
 
   function createCategory(){
     vm.newCategory.$save(function (){
